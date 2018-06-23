e7e12b075a7b58668a8b78f3589dd0a7848a551a
venkataSai <saisiva249@gmail.com>
2018-06-24 00:58:16 +0530

    Creating a task using lamda expression, using the initalization object and storing in the task.asyncstate

--- TaskProgrammingSamples/TaskProgrammingSamples/DeclareAndCallUsingAnonymousFunctions.cs
+++ TaskProgrammingSamples/TaskProgrammingSamples/DeclareAndCallUsingAnonymousFunctions.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿
+using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TaskProgrammingSamples
@@ -9,6 +8,30 @@ namespace TaskProgrammingSamples
     //the other way of accessing and defining the tasks is using anonymous function
     class DeclareAndCallUsingAnonymousFunctions
     {
+        public static void Main()
+        {
+            //from Mian thread
+            Console.WriteLine("Main thread");
+            string Message = "Initial Message";
 
+            var task_NoParameters_NoReturn = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("task_NoParameters_NoReturn");
+            });
+
+
+            //initializing using lamda expression and using a initializing object
+            //initializing object is stored in task.asyncstate, which helps in 
+            //debugging
+            var task_Parameters_NoReturn = Task.Factory.StartNew((state) =>
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine(Message +" and " + state);
+            },"MessageTask");
+
+            task_Parameters_NoReturn.Wait();
+            task_NoParameters_NoReturn.Wait();
+            Console.WriteLine("Main thread ends");
+        }
     }
 }
