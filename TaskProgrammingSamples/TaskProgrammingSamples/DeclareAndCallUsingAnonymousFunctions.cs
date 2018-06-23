31663a2e53b5077cb52d4b00f4678fadb54f9b8b
venkataSai <saisiva249@gmail.com>
2018-06-24 00:14:33 +0530

    Created a class for defining the tasks using anonymous functions

---
+++ TaskProgrammingSamples/TaskProgrammingSamples/DeclareAndCallUsingAnonymousFunctions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskProgrammingSamples
+{
+    //the other way of accessing and defining the tasks is using anonymous function
+    class DeclareAndCallUsingAnonymousFunctions
+    {
+
+    }
+}
